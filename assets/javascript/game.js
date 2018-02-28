52fe730e4e2493a992d77a503b9ca70ea9791888
Joe Finny <finne132@umn.edu>
2018-02-27 18:21:50 -0600

    first commit - basic folder structure and added basic javascript function to generate random letter of the alphabet

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; 
+let answer = Math.floor(Math.random() * 26); 
+
+console.log(alphabet);
+console.log(answer, alphabet[answer]);
