90d19f423abf7d7a581c9d7f61e65dba947abc32
finne132 <finne132@umn.edu>
2018-03-01 12:03:41 -0600

    did javascript scaffolding, declared main variables and initial values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// this is the alphabet in an array, const because it won't change
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// main variables for tracking score/progress/guesses/etc - lets because they will change
+let wins = 0;
+let losses = 0;
+let remainingGuesses = 9;
+let round=0;
+// array to hold letters guessed so far each round
+let soFar = [];
+// this is going to be used later in the actual function to store the key pressed
+let userGuess = null;
